a352530054fe8fac0995e0d03748573fa40125d2
stopwatch <waghamaleshamika2013@gmail.com>
2019-09-26 01:20:26 +0530

    caller id

--- app/src/main/java/com/example/callerid/MainActivity.java
+++ app/src/main/java/com/example/callerid/MainActivity.java
@@ -1,64 +1,54 @@
 package com.example.callerid;
-import android.Manifest;
-import android.net.Uri;
-import android.content.pm.PackageManager;
+import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
-import android.provider.ContactsContract;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
+import android.net.Uri;
 
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    ListView listView;
 
-    private static final int PERMISSIONS_REQUEST_READ_CONTACTS=100;
-    Cursor c;
-    ArrayList<String> contacts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listView=findViewById(R.id.idList);
-
-        int permissionCheck=ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
-        if(permissionCheck==PackageManager.PERMISSION_GRANTED){
-            showContacts();
-        }
-        else{
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_CONTACTS},PERMISSIONS_REQUEST_READ_CONTACTS);
-
-        }
-        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,contacts);
-        listView.setAdapter(adapter);
     }
-    public void onRequestPermissionResult(int requestCode, String[] permissions,int[] grantResults){
-        if (requestCode==PERMISSIONS_REQUEST_READ_CONTACTS){
-            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                showContacts();
-            }
-            else {
-                Toast.makeText(this,"Until you grant the permission , we cannotdisplay the names",Toast.LENGTH_SHORT).show();
-            }
-        }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        return true;
     }
-    private void showContacts(){
-        c=getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI,null,null,null,ContactsContract.Contacts.DISPLAY_NAME+ " ASC ");
-        contacts=new ArrayList<String>();
-        while(c.moveToNext()){
-            String contactName= c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-            String phNumber=c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            contacts.add("Name:"+contactName+"\n"+"PhoneNo:"+ phNumber);
+    public void onClickAddDetails(View view) {
+        ContentValues values = new ContentValues();
+        values.put(UserProvider.name, ((EditText) findViewById(R.id.txtName)).getText().toString());
+        getContentResolver().insert(UserProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), "New Record Inserted", Toast.LENGTH_LONG).show();
+    }
+
+    public void onClickShowDetails(View view) {
+        // Retrieve employee records
+        TextView resultView= (TextView) findViewById(R.id.res);
+        Cursor cursor = getContentResolver().query(Uri.parse("content://com.tutlane.contentprovider.UserProvider/users"), null, null, null, null);
+        if(cursor.moveToFirst()) {
+            StringBuilder strBuild=new StringBuilder();
+            while (!cursor.isAfterLast()) {
+                strBuild.append("\n"+cursor.getString(cursor.getColumnIndex("id"))+ "-"+ cursor.getString(cursor.getColumnIndex("name")));
+                cursor.moveToNext();
+            }
+            resultView.setText(strBuild);
         }
-        c.close();
+        else {
+            resultView.setText("No Records Found");
+        }
     }
 }
